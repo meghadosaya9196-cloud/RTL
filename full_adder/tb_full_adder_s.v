4a991f8984e753d72ef847eb8a678ec5b726fc93
meghadosaya9196-cloud <meghadosaya9196@gmail.com>
2026-03-20 20:23:51 +0530

    Create tb_full_adder_s.v

---
+++ full_adder/tb_full_adder_s.v
@@ -0,0 +1,29 @@
+`timescale 1ns/1ps
+module tb_full_adder;
+reg a,b,cin;
+wire sum,cout;
+
+full_adder dut(.a(a),
+	.b(b),
+	.cin(cin),
+	.sum(sum),
+	.cout(cout));
+
+initial begin
+	//header for console display
+	
+	$display("time  a  b  cin | sum  cout");
+	$display("-------------------------");
+	$monitor("%4t   %b %b  %b | %b   %b ",$time,a,b,cin,sum,cout);
+
+        a=0; b=0; cin=0; #10;
+    	a=0; b=0; cin=1; #10;
+    	a=0; b=1; cin=0; #10;
+    	a=0; b=1; cin=1; #10;
+    	a=1; b=0; cin=0; #10;
+    	a=1; b=0; cin=1; #10;
+    	a=1; b=1; cin=0; #10;
+    	a=1; b=1; cin=1; #10;
+    	$finish;
+ 	end
+endmodule
