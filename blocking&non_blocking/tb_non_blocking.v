351f1bf4afec85c0edacfb18da36de94b1f8c6d9
meghadosaya9196-cloud <meghadosaya9196@gmail.com>
2026-04-03 12:08:19 +0530

    Create tb_non_blocking.v

---
+++ blocking&non_blocking/tb_non_blocking.v
@@ -0,0 +1,33 @@
+`timescale 1ns/1ps
+module tb_non_blocking;
+  
+  reg clk;
+  reg b;
+  wire a;
+  wire c;
+  
+  non_blocking ut(.clk(clk),.b(b),.a(a),.c(c));
+  
+  //clock generation
+  
+  inital begin
+        clk=0;
+    forever #5 clk=~clk;
+  end
+  
+  //stimulus
+  
+  inital begin
+    b=0;
+    #12 b=1;
+    #10 b=0;
+    #10 b=1;
+    #20 $finish;
+    
+  end
+  
+  // monitor
+  $monitor("time=%0t |b=%b a=%b c=%b",$time,b,a,c);
+  end
+  
+endmodule
